823f56582b1f164e85b1cea49b0e56811c5f5ac6
rabea14180-max <rabea14180@gmail.com>
2026-01-14 13:57:17 +0000

    if3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for ch in range(97 ,123):
+for ch in range(97, 123):
     if chr(ch) == "e" or chr(ch) == "q":
-         continue
+        continue
     print("{}".format(chr(ch)), end="")
