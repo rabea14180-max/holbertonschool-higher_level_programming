a998dd8b2ec129140f081798b33b3eef96de9c01
rabea14180-max <rabea14180@gmail.com>
2026-01-14 13:28:13 +0000

    if3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join(chr(i) for i in range(97, 113) if i != 101) + "".join(chr(i) for i in range(114, 123)), end="")
+print("".join(chr(i) for i in range(97, 113) if i != 101) + "".join(chr(i)
+      for i in range(114, 123)), end="")
