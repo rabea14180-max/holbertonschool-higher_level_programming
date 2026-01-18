72e920b37cff7001317d153934930fe1f1043772
rabea14180-max <rabea14180@gmail.com>
2026-01-18 10:18:34 +0000

    p0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-for num in my_list:
-    print("{:d}".format(my_list[num]))
+for i in my_list:
+    print("{:d}".format(my_list[i]))
+    i += 1
 
