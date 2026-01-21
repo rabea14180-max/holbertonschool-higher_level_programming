498cb1f59e0753132975a72fb366ead169b2694d
rabea14180-max <rabea14180@gmail.com>
2026-01-21 10:50:27 +0000

    p2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -21,4 +21,8 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print(f"My name is {first_name} {last_name}".strip())
+    # Print name with or without last name
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
