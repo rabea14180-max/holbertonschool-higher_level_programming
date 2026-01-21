2c4bac1f2e7cad31e77e5b822485c0b26180cf75
rabea14180-max <rabea14180@gmail.com>
2026-01-21 11:30:33 +0000

    p4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Module that defines text_indentation function.
+"""
+
+
+def text_indentation(text):
+    """Print text with 2 new lines after '.', '?' and ':'.
+
+    Args:
+        text (str): The text to be printed.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    separators = ".?:"
+    line = ""
+    i = 0
+    length = len(text)
+
+    while i < length:
+        ch = text[i]
+
+        if ch in separators:
+            
+            line = line.rstrip()
+            line += ch
+            print(line, end="\n\n")
+            line = ""
+            i += 1
+            while i < length and text[i] == " ":
+                i += 1
+            continue
+
+
+        if not line and ch == " ":
+            i += 1
+            continue
+
+        line += ch
+        i += 1
+
+
+    line = line.strip()
+    if line:
+        print(line, end="")
