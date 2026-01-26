1f36d210606ab1c915932013a6788328a7b755fc
rabea14180-max <rabea14180@gmail.com>
2026-01-26 12:52:55 +0300

    p1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines a square with a private size attribute.
+"""
+
+
+class Square:
+    """
+       Represents a square with a private size attribute.
+    """
+
+    def __init__(self, size):
+        self.__size = size
+        
