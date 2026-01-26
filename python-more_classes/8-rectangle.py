36b65cfb90f63b59b35c33b4928649c7a3148f44
rabea14180-max <rabea14180@gmail.com>
2026-01-26 21:10:07 +0300

    Add static method to compare rectangles by area

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -67,3 +67,17 @@ class Rectangle:
         """Print a message when an instance is deleted and update counter."""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        Return the rectangle with the bigger area.
+        If equal, return rect_1.
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
