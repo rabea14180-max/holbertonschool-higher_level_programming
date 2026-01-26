272cfaea5603fd3f8e9059560d9d45ab0d25511f
rabea14180-max <rabea14180@gmail.com>
2026-01-26 08:36:37 +0000

    p0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-    """Defines a Square class."""
+    """
+    Defines a Square class.
+    """
 class Square:
-    """An empty class that defines a square."""
+    """
+        An empty class that defines a square.
+        """
     pass
