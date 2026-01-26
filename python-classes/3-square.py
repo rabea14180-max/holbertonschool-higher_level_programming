377179eec1f04207da0a8a9f6cbcd974be06504e
rabea14180-max <rabea14180@gmail.com>
2026-01-26 13:05:58 +0300

    p3

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Square class with size validation."""
+
+
+class Square:
+    """
+       Represents a square with a private size attribute.
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size ** 2
