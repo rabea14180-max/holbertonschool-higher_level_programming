fa91e63f7c4e65aaf3ec44c780468ce05870de99
rabea14180-max <rabea14180@gmail.com>
2026-01-26 08:30:03 +0000

    p0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-"""
-# This line specifies that the script should be run using Python 3
-"""
-class Square:
-"""
-# This class defines a Square
-    # For now, the class is empty and does not contain
-    # any attributes or methods
-"""
-pass
+"""Defines a Square class."""
 
+class Square:
+    """An empty class that defines a square."""
+    pass
