4d446f03463d4cf2e2b74becbd2453af72510e99
rabea14180-max <rabea14180@gmail.com>
2026-01-27 17:02:52 +0300

    Add inherits_from function and main test file for instance checking

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Return True if obj is an instance of a class that inherited
+    from a_class (directly or indirectly), otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
