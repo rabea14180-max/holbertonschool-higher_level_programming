19bf4b618704aff16b6e544b051f3ddd4ee1fe7e
rabea14180-max <rabea14180@gmail.com>
2026-01-27 11:38:39 +0300

    Fix docstring formatting in lookup function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that defines a function to list attributes and methods of an object."""
+
 def lookup(obj):
-    """Return the list of available attributes and methods of an object"""
+    """Return the list of available attributes and methods of an object."""
     return dir(obj)
