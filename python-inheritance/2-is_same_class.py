d64ca2ae3e5e9227aa4a7d8efb82c779e3a2c169
rabea14180-max <rabea14180@gmail.com>
2026-01-27 16:56:31 +0300

    Refactor docstring in is_same_class function for clarity and consistency

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,5 +3,5 @@
 
 
 def is_same_class(obj, a_class):
-    """Returns True if obj is exactly an instance of a_class, otherwise False"""
+    """Return True if obj is exactly an instance of a_class, else False."""
     return type(obj) is a_class
