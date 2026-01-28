b9565ef359447c062462ee16dcf665ce184fd9e4
rabea14180-max <rabea14180@gmail.com>
2026-01-28 12:11:27 +0300

    Refactor docstring in 7-base_geometry.py for clarity and remove unused area method

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 """
-This module defines a BaseGeometry class with validation.
+Defines a BaseGeometry class
 """
 
 
 class BaseGeometry:
     """BaseGeometry class"""
 
-    def area(self):
-        """Method that should be implemented by subclasses"""
-        raise Exception("area() is not implemented")
-
     def integer_validator(self, name, value):
-        """Validates that value is an integer greater than 0"""
+        """Validates value as a positive integer"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
