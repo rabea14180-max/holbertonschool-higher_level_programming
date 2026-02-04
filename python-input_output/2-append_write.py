a364fafd22daaf2f952b1f38e2e6656ec4f343c7
rabea14180-max <rabea14180@gmail.com>
2026-02-04 16:10:22 +0300

    Add append_write function for appending text to a file with documentation

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.2-append_write
+"""
+
+
+def append_write(filename="", text=""):
+    """
+        Docstring for append_write
+        write a text
+        :param filename: Description
+        :param text: Description
+        """
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
+    
