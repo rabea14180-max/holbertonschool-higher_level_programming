8c075423a91136b186a541c2557d74f4cefe9a51
rabea14180-max <rabea14180@gmail.com>
2026-02-04 16:41:30 +0300

    Add Student class with initialization and JSON representation methods

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only those attributes are returned.
+        """
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+        return self.__dict__
