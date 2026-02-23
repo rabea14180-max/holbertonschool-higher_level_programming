eb9dab744fbbb86a68600bc5a399d8aafaecbfc6
rabea14180-max <rabea14180@gmail.com>
2026-02-23 06:42:33 +0300

    T6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Create table states if it does not exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
