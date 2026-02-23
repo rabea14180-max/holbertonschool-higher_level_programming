c9e87677aa2a54010b28ee59a192a0e39eaac136
rabea14180-max <rabea14180@gmail.com>
2026-02-23 07:06:00 +0300

    T8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
+-- Lists all cities of California without using JOIN
 SELECT cities.id, cities.name
 FROM cities
-WHERE cities.state_id IN (
+WHERE cities.state_id = (
     SELECT states.id
     FROM states
     WHERE states.name = 'California'
