8f1300f356fcdc8ec6db34cccffd889fd01aa01e
rabea14180-max <rabea14180@gmail.com>
2026-02-23 06:29:57 +0300

    T1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Create user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
