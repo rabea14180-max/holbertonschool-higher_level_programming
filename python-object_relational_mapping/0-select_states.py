08b8a5a4e12103b77d01c96e33c93791c766170f
rabea14180-max <rabea14180@gmail.com>
2026-02-24 10:20:28 +0300

    Refactor script documentation and improve query execution

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-"""List all states from the database hbtn_0e_0_usa."""
+"""
+Lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>
+"""
 
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
+        db=sys.argv[3]
     )
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    for row in cur.fetchall():
+    # Create cursor
+    cursor = db.cursor()
+
+    # Execute query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all rows
+    results = cursor.fetchall()
+
+    # Print results
+    for row in results:
         print(row)
 
-    cur.close()
+    # Close cursor and connection
+    cursor.close()
     db.close()
     
