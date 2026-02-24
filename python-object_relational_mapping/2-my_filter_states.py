11b695774cd9549a08ebc40c06a6d9aa5e32dc6d
rabea14180-max <rabea14180@gmail.com>
2026-02-24 10:51:31 +0300

    Refactor: Improve module documentation and restructure main function

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table
-where name matches the given argument
-Usage:
-./2-my_filter_states.py <mysql_username> <mysql_password> <database_name> <state_name>
+This module filters states by name from a MySQL database.
+
+It connects to a MySQL server running on localhost at port 3306,
+queries the states table, and displays rows where the name matches
+the provided argument. Results are sorted by states.id in ascending order.
 """
 
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    # Connect to MySQL server
+def main():
+    """
+    Connects to the MySQL database and retrieves states
+    that match the provided name argument.
+    """
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -22,8 +26,9 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    # Create query using format (as required)
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+        sys.argv[4]
+    )
     cursor.execute(query)
 
     results = cursor.fetchall()
@@ -33,3 +38,7 @@ if __name__ == "__main__":
 
     cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
