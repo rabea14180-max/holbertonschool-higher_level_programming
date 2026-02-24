6d2e3de937e2eb1dfb7e8f5a9e8308be53efdfe7
rabea14180-max <rabea14180@gmail.com>
2026-02-24 14:15:34 +0300

    Refactor: Simplify database connection and query execution in filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,41 @@
 #!/usr/bin/python3
+"""
+Lists all states with a name starting with N (upper N) 
+from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Get arguments from command line
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
+        user=mysql_user,
+        passwd=mysql_password,
+        db=db_name
     )
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    # Create a cursor object to execute queries
+    cursor = db.cursor()
 
-    for row in cur.fetchall():
+    # Execute the SQL query
+    # BINARY ensures the search is case-sensitive for the uppercase 'N'
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
         print(row)
 
-    cur.close()
+    # Clean up
+    cursor.close()
     db.close()
