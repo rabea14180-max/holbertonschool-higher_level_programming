e404da1028dab58da92bfde38b1049d0906bec06
rabea14180-max <rabea14180@gmail.com>
2026-02-24 14:11:59 +0300

    Refactor: Simplify variable usage and remove unnecessary comments in filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name starting with 'N' (upper N)
-from the specified MySQL database.
-"""
-
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         charset="utf8"
     )
 
-    # Create cursor
     cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
 
-    # Execute query
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    )
-
-    # Fetch and display results
     for row in cur.fetchall():
         print(row)
 
-    # Close connections
     cur.close()
     db.close()
