98abc0269dea0a35395d096c69c4d98831f51473
rabea14180-max <rabea14180@gmail.com>
2026-03-30 13:24:17 +0300

    Add: Implement toggle functionality for header color change

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,10 +1,3 @@
-document.querySelector('#toggle_header').addEventListener('click', function() {
-  const header = document.querySelector('header');
-  if (header.classList.contains('red')) {
-    header.classList.remove('red');
-    header.classList.add('green');
-  } else {
-    header.classList.remove('green');
-    header.classList.add('red');
-  }
+document.querySelector('#red_header').addEventListener('click', function() {
+  document.querySelector('header').classList.add('red');
 });
